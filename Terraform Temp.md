0a1cefe4d1bdb1478719275f0bff2896f05a05dc
domino659 <martin@sionfamily.com>
2022-02-14 16:19:23 +0100

    Init

---
+++ Terraform Temp.md
@@ -0,0 +1,6 @@
+{
+"appId": "a172e684-cade-444d-8b0d-acd72fc709a8",
+"displayName": "Terraform-Temp",
+"password": "e17RgKDuT7ZCVfbhQ4IN1mHt_3-y~iIKGz",
+"tenant": "86ea90ab-dda8-4ce4-99ae-b9c4c7a4ad03"
+}
---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "Azure region"
+  default     = "westeurope"
+}
+
+variable "resource_group_name" {
+  default = "ONYX2"
+}
+
+variable "ressource_virtual_network_name" {
+  default = "VPN_AzureCarac"
+}
